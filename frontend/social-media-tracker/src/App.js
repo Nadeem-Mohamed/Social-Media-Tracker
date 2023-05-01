67ce844b3949bdd61b1b98dc447464a22c0c0e7d
Nadeem Mohamed <nadeem.yaqub06@gmail.com>
2023-05-01 13:47:09 -0400

    beginning

--- frontend/social-media-tracker/src/App.js
+++ frontend/social-media-tracker/src/App.js
@@ -2,12 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const names=["Nadeem", "Mohil", "Gurpal"]
+  const names=[
+    {name: "Nadeem", age: 17},
+    {name: "Mohil", age: 16},
+    {name: "Gurpal",age:  21}
+  ]
   return (
     <div className="App">
-       
+      {names.map((details,key)=> {
+       return <Group name={details.name} age={details.age}></Group>})}
     </div>
   );
 }
-
+const Group=(props) => {
+  return(
+    <div>
+      
+        {props.name}
+        {props.age}
+    </div>)
+}
 export default App;
