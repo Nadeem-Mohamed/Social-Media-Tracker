4270b4cf20d75cb9a0e4f2e1f95381e3507760f1
Nadeem Mohamed <nadeem.yaqub06@gmail.com>
2023-05-02 10:57:11 -0400

    Added a navbar

--- frontend/social-media-tracker/src/App.js
+++ frontend/social-media-tracker/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/navbar.js'
 function App() {
-  const names=[
-    {name: "Nadeem", age: 17},
-    {name: "Mohil", age: 16},
-    {name: "Gurpal",age:  21}
-  ]
+  
   return (
     <div className="App">
-      {names.map((details,key)=> {
-       return <Group name={details.name} age={details.age}></Group>})}
+      {Navbar()}
+
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+
     </div>
   );
 }
 const Group=(props) => {
-  return(
-    <div>
-      
-        {props.name}
-        {props.age}
-    </div>)
+  return
 }
 export default App;
---
+++ frontend/social-media-tracker/src/components/navbar.js
@@ -0,0 +1,9 @@
+import './navbar.css'
+export default function Navbar(){
+    return(
+    <nav className='navigation' >
+        <a href="/" className="brand-name">
+            Social Media Tracker
+          </a>
+    </nav>)
+}
