05639ff6ed3dae8bd46f2a1f2fc10a522a66d5e5
Nadeem Mohamed <nadeem.yaqub06@gmail.com>
2023-05-06 10:15:04 -0400

    added about page

--- frontend/social-media-tracker/src/pages/About.js
+++ frontend/social-media-tracker/src/pages/About.js
@@ -1,7 +1,11 @@
+import './about.css';
+
 export default function About() {
-	return (
-		<div className="About">
-			<h1>About</h1>
-		</div>
-	)
+  return (
+    <div className="About">
+      <h1>Welcome to our website, where we help businesses and individuals track their social media performance across multiple platforms. </h1>
+      <p>Our platform provides comprehensive insights into your social media performance, including tracking your likes and followers, and presenting the data in easy-to-understand graphs and charts. With our tools, you can measure your social media success and adjust your strategies accordingly.</p>
+      <p>We are passionate about helping our clients succeed on social media, and we strive to provide the most accurate and up-to-date data to help you make informed decisions. Join us on our mission to make social media analytics accessible and understandable to everyone.</p>
+    </div>
+  )
 }
