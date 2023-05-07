8accbfbf98b2004331a692cc0cbb005edb577098
Nadeem Mohamed <nadeem.yaqub06@gmail.com>
2023-05-07 14:27:28 -0400

    modified login page to include google signin

--- frontend/social-media-tracker/src/pages/Login.js
+++ frontend/social-media-tracker/src/pages/Login.js
@@ -1,7 +1,20 @@
-export default function Login() {
-	return (
-		<div className="Login">
-			<h1>Login</h1>
-		</div>
-	)
+import React from 'react';
+import './login.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Google from './google';
+
+function login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+
+        <GoogleOAuthProvider clientId="598251810358-o6mie5a7g6lpho39340coo9g7mlj2ved.apps.googleusercontent.com">
+          <Google />
+        </GoogleOAuthProvider>
+      </header>
+    </div>
+  );
 }
+
+export default login;
