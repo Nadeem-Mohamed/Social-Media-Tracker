e135fb3f4fba007f819ed5ed23a2b4552f21dfb2
Nadeem Mohamed <nadeem.yaqub06@gmail.com>
2023-06-05 10:45:47 -0400

    added graph

--- frontend/social-media-tracker/src/pages/Stats.js
+++ frontend/social-media-tracker/src/pages/Stats.js
@@ -1,3 +1,24 @@
-export default function Stats() {
+import XYPlot from 'reactochart/XYPlot';
+import XAxis from 'reactochart/XAxis';
+import YAxis from 'reactochart/YAxis';
+import LineChart from 'reactochart/LineChart';
+import 'reactochart/styles.css';
 
+
+export default function Stats() {
+    const data = [
+        {x: 0, y: 2},
+        {x: 5, y: 22},
+        {x: 10, y: 32},
+        {x: 15, y: 36},
+      ];
+      return <XYPlot width={400} height={300}>
+        <XAxis showGrid={false} title="Years" />
+        <YAxis title="Followers"/>
+        <LineChart
+          data={data}
+          x={d => d.x}
+          y={d => d.y}
+        />
+      </XYPlot>
 }
