4a12d98ef93e2551795f66950d52a1ab676793a2
lbcmk <30442978+lbcmk@users.noreply.github.com>
2023-06-04 21:23:56 -0400

    Experimenting for firestore

--- frontend/social-media-tracker/src/App.js
+++ frontend/social-media-tracker/src/App.js
@@ -6,9 +6,51 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Login from "./pages/Login.js";
+import Stats from './pages/Stats.js';
+
+import Authentication from './functions/Authentication';
+import {auth, app, db} from './firebase-config'
+import { collection, doc, query, setDoc, where, getDoc } from 'firebase/firestore';
 
 function App() {
   
+  // START
+  const changeUser = () => {
+    var userInfo = Authentication();
+    // console.log(userInfo.uid)  
+    if(userInfo) {
+      const users = collection(db, "test")
+      try {
+        var q = 0;
+        const test = async() => {
+          q = await getDoc(doc(db, `/test/${userInfo.uid}`))
+          return q
+        }
+
+        // const w = setDoc(doc(db, `/test/${userInfo.uid}`), {
+        //   useruid: userInfo.uid,
+        //   calendarEvents: []
+        // })
+        test().then(() => {
+          console.log(q.exists);
+        //   if(q == undefined || q == null) {
+        //   }
+          
+        //   test().then(() => {
+        //     console.log(q);
+        //   });
+        });
+        
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
+  let username = changeUser() || false
+
+  // END
+
   return (
     <div className="App">
       <div className="Navbar">
@@ -20,6 +62,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/stats" element={<Stats />} />
         </Routes>
       </div>
 
